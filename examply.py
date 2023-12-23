88f458e131e2a3194a65f7cb36a5a8cf7af6dd20
tictac22 <fominsartems947@gmail.com>
2023-12-23 22:07:00 +0200

    Get salary info

--- examply.py
+++ examply.py
@@ -16,3 +16,21 @@ option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
 driver.get('https://hop.visma.lv/#!/login')
+
+driver.implicitly_wait(1)
+
+input_email = driver.find_element(By.ID, "username")
+input_password = driver.find_element(By.ID, "password_hidden")
+
+email = os.environ.get("email")
+input_email.send_keys(email)
+
+password = os.environ.get("password")
+input_password.send_keys(password)
+
+input_email.submit()
+
+
+salary_box =  WebDriverWait(driver,6).until(EC.presence_of_element_located((By.XPATH,"//h2[text()='Mana alga']")))
+salary_box.click()
+time.sleep(5)
