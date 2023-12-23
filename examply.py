174d8e4a53e8552627e2c991e9ad4b084a760941
tictac22 <fominsartems947@gmail.com>
2023-12-23 23:04:32 +0200

    Resolve pylint issues

--- examply.py
+++ examply.py
@@ -1,7 +1,8 @@
+"""Module for getting enviroment variables"""
 import os
 import time
-from datetime import datetime
 from dotenv import load_dotenv
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -10,14 +11,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 load_dotenv()
 
-currentMonth = datetime.now().month
 service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
 driver.get('https://hop.visma.lv/#!/login')
 
-driver.implicitly_wait(1)
+time.sleep(1)
 
 input_email = driver.find_element(By.ID, "username")
 input_password = driver.find_element(By.ID, "password_hidden")
@@ -31,10 +31,17 @@ input_password.send_keys(password)
 input_email.submit()
 
 
-salary_box =  WebDriverWait(driver,6).until(EC.presence_of_element_located((By.XPATH,"//h2[text()='Mana alga']")))
+salary_box =  WebDriverWait(driver,6).until(EC.presence_of_element_located((
+    By.XPATH,
+    "//h2[text()='Mana alga']"
+)))
 salary_box.click()
 
-salary = WebDriverWait(driver,6).until(EC.presence_of_element_located((By.XPATH,'//span[text()="Izmaksa:"]/following-sibling::span')))
+salary = WebDriverWait(driver,6).until(EC.presence_of_element_located((
+    By.XPATH,
+    '//span[text()="Izmaksa:"]/following-sibling::span'
+)))
 salary = float(salary.text[4:].replace(",","."))
+
 print(salary)
 time.sleep(5)
